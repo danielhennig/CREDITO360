684c723f70677f14f553186710905ea7057d4511
danielhennig <danielhennig10@gmail.com>
2025-06-10 20:10:18 -0300

    Remove backend implementation for BancoBanrisul, including all controllers, models, routes, migrations, seeders, and configuration files. This commit effectively deletes the entire backend structure for the project.

--- BancoBanrisul/BanrisulBackEnd/src/routes/contaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contaController = require('../controllers/contaController.js');
-
-router.post('/', contaController.criarConta);
-router.get('/', contaController.listarContas);
-
-module.exports = router;
--- BancoBanrisul/BanrisulBackEnd/src/routes/openFinanceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware');
-const controller = require('../controllers/openFinanceController');
-
-router.get('/dados',  controller.dadosBancarios);
-
-module.exports = router;
