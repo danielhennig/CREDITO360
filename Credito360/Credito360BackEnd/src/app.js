0e5ed04b29e3fea592df2951d1100bd29f04626a
danielhennig <danielhennig10@gmail.com>
2025-06-12 15:37:05 -0300

    refactor(routes): remove rotas e controlador de dados bancários

--- Credito360/Credito360BackEnd/src/app.js
+++ Credito360/Credito360BackEnd/src/app.js
@@ -10,14 +10,12 @@ app.use(cors());
 // Importação das rotas
 const clienteRoutes = require('./routes/clienteRoutes');
 const authRoutes = require('./routes/authRoutes');
-const dadosBancariosRoutes = require('./routes/dadosBancariosRoutes');
 const conexaoBancariaRoutes = require('./routes/conexaoBancariaRoutes');
 const scoreRoutes = require('./routes/scoreRoutes');
 
 // Prefixo padrão: /credito360
 app.use('/credito360', clienteRoutes);
 app.use('/credito360', authRoutes);
-app.use('/credito360', dadosBancariosRoutes);
 app.use('/credito360', conexaoBancariaRoutes);
 app.use('/credito360', scoreRoutes);
 
--- Credito360/Credito360BackEnd/src/routes/dadosBancariosRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dadosBancariosController = require('../controllers/dadosBancariosController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.post('/dados-bancarios',  dadosBancariosController.receberDados);
-
-module.exports = router;
